4090e47796b7769f016124008d2ee004f7181972
DeDuShKa12 <pasharudiuk97@gmail.com>
2022-11-01 01:18:30 +0300

    Lesson 1 JS HW1 done

---
+++ Lesson 1/main.js
@@ -0,0 +1,46 @@
+let a1
+    a1 = 'hello'
+console.log(a1)
+let b1
+    b1 = 'owu'
+console.log(b1)
+let c1
+    c1 = 'com'
+console.log(c1)
+let d
+    d = 'ua'
+console.log(d)
+let e
+    e = 1
+console.log(e)
+let f = 10;
+console.log(f)
+let g = -999;
+console.log(g)
+let h = 123;
+console.log(h)
+const PI = 3.14;
+console.log(PI)
+let i = 2.7;
+console.log(i)
+let j = 16
+console.log(j)
+let k = true
+console.log(k)
+let l = false
+console.log(l)
+
+
+let firstName = 'Павло'
+let middleName = 'Олександрович'
+let lastName = 'Рудюк'
+let msg = `${firstName} ${middleName} ${lastName}`
+console.log(msg)
+
+
+let a = 100;
+console.log(typeof a, a)
+let b = '100';
+console.log(typeof b, b)
+let c = true;
+console.log(typeof c, c)
