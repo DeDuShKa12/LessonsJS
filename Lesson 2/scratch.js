eb6f10dcec5c846853d8080d304eaa2d8ea72892
DeDuShKa12 <pasharudiuk97@gmail.com>
2022-11-03 18:11:40 +0300

    Lesson 2 JS HW done

---
+++ Lesson 2/scratch.js
@@ -0,0 +1,77 @@
+let arr = [1, 2, 3, 4, 5, '6', 7, 8, 9, [10, 9, 1]]
+console.log(arr)
+
+
+let book1 = {
+    title: 'Sword of Destiny',
+    pageCount: 384,
+    genre: 'Fantasy',
+}
+let book2 = {
+    title: 'The Last Wish',
+    pageCount: 288,
+    genre: 'Fantasy',
+}
+let book3 = {
+    title: 'Blood of Elves',
+    pageCount: 320,
+    genre: 'Fantasy',
+}
+console.log(book1, book2, book3)
+
+
+let book11 = {
+    title: 'Sword of Destiny',
+    pageCount: 384,
+    genre: 'Fantasy',
+    authors: [
+        autor1 = {name: 'Andrzej', age: 74},
+        autor2 = {name: 'Sapkowski', age: 75}
+    ]
+}
+let book22 = {
+    title: 'The Last Wish',
+    pageCount: 288,
+    genre: 'Fantasy',
+    authors: [
+        autor1 = {name: 'Andrzej', age: 74},
+        autor2 = {name: 'Sapkowski', age: 75}
+    ]
+}
+let book33 = {
+    title: 'Blood of Elves',
+    pageCount: 320,
+    genre: 'Fantasy',
+    authors: [
+        autor1 = {name: 'Andrzej', age: 74},
+        autor2 = {name: 'Sapkowski', age: 75}
+    ]
+}
+console.log(book11, book22, book33)
+
+
+
+let users = [
+    user1 = {name: 'adw', username: 'dawdf',password: 3391},
+    user2 = {name: 'daw', username: 'vzdv',password: 12312},
+    user3 = {name: 'zvvz', username: 'zvzvc',password: 3141},
+    user4 = {name: 'fsfg', username: 'bxfb',password: 31431},
+    user5 = {name: 'sefe', username: 'ncgn',password: 132},
+    user6 = {name: 'xvfb', username: 'vevgrs',password: 6745},
+    user7 = {name: 'dfds', username: 'vsevs',password: 7547},
+    user8 = {name: 'vxdv', username: 'vesv',password: 8658},
+    user9 = {name: 'bdbfd', username: 'xcbx',password: 8658},
+    user10 = {name: 'nhfn', username: 'rwtwrh',password: 86585},
+
+]
+
+console.log(users[0].password)
+console.log(users[1].password)
+console.log(users[2].password)
+console.log(users[3].password)
+console.log(users[4].password)
+console.log(users[5].password)
+console.log(users[6].password)
+console.log(users[7].password)
+console.log(users[8].password)
+console.log(users[9].password)
