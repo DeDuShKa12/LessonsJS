e98fb5a02f548d8775978283681de99dd6f66b4c
DeDuShKa12 <pasharudiuk97@gmail.com>
2022-11-10 15:28:14 +0300

    Lesson 4 JS HW done

---
+++ Lesson 4/main.js
@@ -0,0 +1,353 @@
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}]
+//
+// for (let i = 0; i < users.length - 1; i++) {
+//     document.write(`<div class="name">${users[i].name}</div>`)
+// }
+
+
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+//
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}]
+//
+// for (let i = 0; i < users.length - 1; i++) {
+//     document.write(`<div class="name">${i}
+// ${users[i].name}
+// </>`)}
+
+
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+//
+//
+// let locations = [
+//     {
+//         "title": "м. Київ",
+//         "type": "city"
+//     },
+//     {
+//         "title": "Житомирська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Харківська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Вінницька область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Чернігівська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Дніпропетровська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Черкаська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Волинська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Київська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Миколаївська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Рівненська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Сумська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Тернопільська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Краматорський район",
+//         "type": "raion"
+//     },
+//     {
+//         "title": "Кіровоградська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Хмельницька область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Запорізька область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Одеська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Полтавська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Львівська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "м. Кривий Ріг",
+//         "type": "city"
+//     },
+//     {
+//         "title": "Криворізька територіальна громада",
+//         "type": "hromada"
+//     },
+//     {
+//         "title": "Івано-Франківська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Чернівецька область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Ізюмський район",
+//         "type": "raion"
+//     }]
+//
+// let i = 0
+// while (i < locations.length - 5)
+// {
+//     let location = locations[i]
+//     document.write(`<h1>${location.title}</h1>`)
+//     i++
+// }
+
+
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+//
+// let locations = [
+//     {
+//         "title": "м. Київ",
+//         "type": "city"
+//     },
+//     {
+//         "title": "Житомирська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Харківська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Вінницька область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Чернігівська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Дніпропетровська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Черкаська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Волинська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Київська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Миколаївська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Рівненська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Сумська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Тернопільська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Краматорський район",
+//         "type": "raion"
+//     },
+//     {
+//         "title": "Кіровоградська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Хмельницька область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Запорізька область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Одеська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Полтавська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Львівська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "м. Кривий Ріг",
+//         "type": "city"
+//     },
+//     {
+//         "title": "Криворізька територіальна громада",
+//         "type": "hromada"
+//     },
+//     {
+//         "title": "Івано-Франківська область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Чернівецька область",
+//         "type": "oblast"
+//     },
+//     {
+//         "title": "Ізюмський район",
+//         "type": "raion"
+//     }]
+//
+// let i = 0
+// while (i < locations.length - 5)
+// {
+//     let location = locations[i]
+//     document.write(`<h1>${i} ${location.title}</h1>`)
+//     i++
+// }
+
+
+// - Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+//
+// let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+//
+// for (let i = 0; i < listOfItems.length; i++) document.write(`<ul><li>${listOfItems[i]}</li></ul>`)
+
+
+// Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону  Зробити адекватну стилізацію
+// Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+//
+// let products = [
+//     {
+//         title: 'milk',
+//         price: 22,
+//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+//     },
+//     {
+//         title: 'juice',
+//         price: 27,
+//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+//     },
+//     {
+//         title: 'tomato',
+//         price: 47,
+//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+//     },
+//     {
+//         title: 'tea',
+//         price: 15,
+//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+//     },
+// ];
+//
+// for (let i = 0; i < products.length; i++) document.write(`
+// <div class="product-card">
+//         <h3 class="product-title">Titel - ${products[i].title}. Price - ${products[i].price}</h3>
+//         <img src="${products[i].image}" alt="${products[i].title}" class="product-image">
+// </div>`)
+
+
+// ----------------------є масив---------------------
+
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+// за допомоги циклу вивести:
+
+//- користувачів зі статусом true
+//
+// for (let user of users) {
+//     if (user.status) {
+//         document.write(`<ul><li>${user.name}</li></ul>`)
+//     }
+//
+// }
+
+
+// - користувачів зі статусом false
+// for (let user of users) {
+//     if (!user.status) {
+//         document.write(`<ul><li>${user.name}</li></ul>`)
+//     }
+//
+// }
+
+
+// - користувачів які старші за 30 років
+// for (let user of users) {
+//     if (user.age > 30) {
+//         document.write(`<ul><li>${user.name}</li></ul>`)
+//     }
+//
+// }
